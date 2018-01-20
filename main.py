068aa76262daf8f6085c9db843adc780f9b3b95a
gregvjames99 <gregvjames99@gmail.com>
2018-01-20 16:06:50 -0500

    Added all files

---
+++ main.py
@@ -0,0 +1,14 @@
+import genData
+#import prettyPrint
+#import SimpleHTTPServer
+#import SocketServer
+
+#PORT = 8000
+
+#Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+#httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+#print "serving at port", PORT
+
+#httpd.serve_forever()
