de2d2a1b3e5fc8b2aec607dbea47e5d8854b0cbf
gregvjames99 <gregvjames99@gmail.com>
2018-01-20 22:14:51 -0500

    Added last tweet graph, modularized graps

--- genData.py
+++ genData.py
@@ -14,19 +14,33 @@ access_token_secret = parser.get('API_KEYS', 'access_token_secret')
 
 auth = tweepy.OAuthHandler(consumer_key , consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-db = TinyDB('db.json')
-db.purge()
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
 
-friends = api.followers_ids()
-
-count = 0;
-
-bar = progressbar.ProgressBar(redirect_stdout=True, max_value=len(friends))
-for id in friends:
+def fillDB():
+  db = TinyDB('db.json')
+  db.purge()
+  friends = api.followers_ids()
+  count = 0;
+  bar = progressbar.ProgressBar(redirect_stdout=True, max_value=len(friends))
+  for id in friends:
     user = api.get_user(id)
     db.insert({'id': user.id, 'location': user.location, 'followers_count': user.followers_count, 'statuses_count': user.statuses_count, 'friends_count': user.friends_count, 'screen_name': user.screen_name, 'lang': user.lang, 'following': user.following, 'time_zone':user.time_zone, 'created_at': user.created_at, 'default_user': user.default_profile, 'default_profile_image': user.default_profile_image})
     count = count + 1
     time.sleep(0.1)
     bar.update(count)
-
+    
+def get_last_tweet(id):
+    tweet = None
+    
+    try:
+      tweet = (api.user_timeline(id = id, count = 1, include_rts = False))[0];
+    except:
+      tweet = None
+      
+    if tweet != None:
+      return tweet.created_at
+    else:
+      return None
+    
+#COMMENT THIS OUT IF YOU DONT WANT THE DB TO BE UPDATED
+fillDB();
--- prettyPrint.py
+++ prettyPrint.py
@@ -2,47 +2,106 @@ from tinydb import TinyDB, Query
 from collections import Counter
 import plotly
 import plotly.graph_objs as go
+from genData import get_last_tweet
+import time
+import progressbar
 
 db = TinyDB('db.json')
 
 #USER LANGS
-a = []
-for item in db:
-  a.append(item['lang'])
+def User_Lang():
+  a = []
+  for item in db:
+    a.append(item['lang'])
 
-keys = list(Counter(a).keys())
-vals = list(Counter(a).values())
+  keys = list(Counter(a).keys())
+  vals = list(Counter(a).values())
 
-trace = go.Pie(labels=keys, values=vals)
-plotly.offline.plot([trace], filename='basic_pie_chart.html')
+  trace = go.Pie(labels=keys, values=vals)
+  plotly.offline.plot([trace], filename='basic_pie_chart.html')
 
-print("Created language pie chart")
+  print("Created language pie chart")
 
 #HOW MANY USERS ARE YOU FOLLOWING BACK
-b = []
-for item in db:
-  b.append(item['following'])
+def Follow_back():
+  b = []
+  for item in db:
+    b.append(item['following'])
 
-vals = list(Counter(b).values())
+  vals = list(Counter(b).values())
 
-data = go.Bar(x=["false","true"],y=vals)
-plotly.offline.plot([data], filename='basic_bar_chart.html')
+  data = go.Bar(x=["false","true"],y=vals)
+  plotly.offline.plot([data], filename='basic_bar_chart.html')
 
-print("Created your following bar chart")
+  print("Created your following bar chart")
 
 #DEFAULT PROFILE IMAGE
-c = []
-d = []
-e = []
+def Default_profile():
+  c = []
+  d = []
+  e = []
 
-for item in db:
-  if item['default_profile_image'] is True:
-    c.append(item['screen_name'])
-    d.append(item['id'])
-    e.append(item['default_profile_image'])
+  for item in db:
+    if item['default_profile_image'] is True:
+      c.append(item['screen_name'])
+      d.append(item['id'])
+      e.append(item['default_profile_image'])
     
-trace = go.Table(header=dict(values=['Screen Name', 'ID', 'default_profile_image']),cells=dict(values=[c, d, e]))
+  trace = go.Table(header=dict(values=['Screen Name', 'ID', 'default_profile_image']),cells=dict(values=[c, d, e]))
 
-data = [trace] 
-plotly.offline.plot(data, filename='basic_table_html.html')
-print("Created Table")
+  data = [trace] 
+  plotly.offline.plot(data, filename='basic_table_html.html')
+  print("Created Table")
+
+#Last tweet
+def Tweet_Line():
+  f = []
+  g = []
+  h = []
+  notweet_user = []
+
+  count = 0;
+
+  bar = progressbar.ProgressBar(redirect_stdout=True, max_value=len(db))
+  for item in db:
+    last_tweet = get_last_tweet(item['id']);
+    if(last_tweet != None):
+      f.append(item['screen_name'])
+      g.append(last_tweet);
+      h.append(item['statuses_count'])
+      #print(f[count])
+      #print(g[count].year)
+    else:
+      notweet_user.append(item['screen_name'])
+      #print(f[count])
+      #print(g[count])
+    count = count + 1
+    time.sleep(0.1)
+    bar.update(count)
+
+  data = [go.Scatter(x=g,y=h,mode='markers',text=f)]
+
+  layout = dict(
+      title='User last tweet time',
+      xaxis=dict(
+          rangeselector=dict(buttons=list([
+                dict(count=1,label='1d',step='day',stepmode='backward'),
+                dict(count=1,label='1m',step='month',stepmode='backward'),
+                dict(count=6,label='6m',step='month',stepmode='backward'),
+                dict(step='all')
+          ])),
+      rangeslider=dict(),
+      type='date'
+      )
+  )
+  fig = dict(data=data, layout=layout)
+
+  plotly.offline.plot(fig, filename='linechart.html')
+  print("Created user last tweet line chart!")
+  
+
+#DEFINE WHAT GRAPHS YOU WANT HERE BY CALLING THE FXN
+Follow_back();
+Default_profile();
+User_Lang();
+Tweet_Line();
